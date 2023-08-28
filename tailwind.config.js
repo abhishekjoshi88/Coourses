ab7a5cc376aa65bc1a90df6178b3ca98c37bcb4b
abhishekjoshi88 <abhishek11joshi11@gmail.com>
2023-08-28 12:26:38 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: "#141414",
+        bgDark2: "#dcdcdc",
+      },
+    },
+  },
+  plugins: [],
+};
